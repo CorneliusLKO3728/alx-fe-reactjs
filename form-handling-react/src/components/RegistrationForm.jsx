d78aa77bab7e8c17f9e87d711b8fea608a73462a
CorneliusLKO3728 <lukecorners6@gmail.com>
2025-11-02 20:58:37 +0300

    new

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,19 +1,27 @@
-
 import React, { useState } from 'react';
 
 const RegistrationForm = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [errors, setErrors] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!username || !email || !password) {
-      setError('All fields are required!');
+    if (!username) {
+      setErrors('Username is required');
       return;
     }
-    setError('');
+    if (!email) {
+      setErrors('Email is required');
+      return;
+    }
+    if (!password) {
+      setErrors('Password is required');
+      return;
+    }
+
+    setErrors('');
     console.log('Form Submitted:', { username, email, password });
   };
 
@@ -42,7 +50,7 @@ const RegistrationForm = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
 
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {errors && <p style={{ color: 'red' }}>{errors}</p>}
 
       <button type="submit">Register</button>
     </form>
